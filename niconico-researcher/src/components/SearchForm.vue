9ee88b9296560d24a82b35c6b584d8244fb9fab6
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-06-18 19:24:36 +0900

    fix: 検索フォームのcssを整える

--- niconico-researcher/src/components/SearchForm.vue
+++ niconico-researcher/src/components/SearchForm.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="search_form_view">
-
+        <div class="search_form_input_area">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            検索したい配信者の名前を入力
+        </div>
     </div>
 </template>
 
@@ -11,4 +14,19 @@
         background-color: lightgray;
         margin: 0 16px 16px 16px;
     }
+
+    .search_form_input_area {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: left;
+        margin-left: 8px;
+        padding-left: 8px;
+    }
+
+    .search_form_input_area i {
+        margin-right: 8px;
+    }
+
 </style>
