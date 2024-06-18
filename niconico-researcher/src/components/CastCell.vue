31f0b86ce139a4845746baa35a2c4ff604b58ba2
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-06-18 22:56:28 +0900

    fix: 配信中アイコンの分岐

--- niconico-researcher/src/components/CastCell.vue
+++ niconico-researcher/src/components/CastCell.vue
@@ -3,7 +3,7 @@
         <div class="cell_user_image_area">
             <div class="user_icon_container">
                 <div class="icon user_image_icon"></div>
-                <div class="icon user_is_broadcasting_icon">
+                <div v-if="userData.platform[0].isBroadCasting == true" class="icon user_is_broadcasting_icon">
                     <i class="fa-solid fa-video fa-fade"></i>
                 </div>
             </div>
