3967c34251e9d2262ac22bb9a580a57df47667b3
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-06-19 17:01:51 +0900

    fix: hover時の処理を実装

--- niconico-researcher/src/components/CategoryList/SingleCategory.vue
+++ niconico-researcher/src/components/CategoryList/SingleCategory.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="single_category_tab" v-for="(item, key) in platformlist" :key="key">
-        <div class="itemContainer">
+        <div class="itemContainer"
+            :style="itemStyle(key)" 
+             @mouseover="hoveredIndex = key" 
+             @mouseleave="hoveredIndex = null"
+        >
             <div v-if="item.serviceId === 2" class="category_name">
                 ふ
             </div>
@@ -35,8 +39,27 @@ export default {
           {text: Platform.Services.TikTok.label, color: "TikTokColor", icon: Platform.Services.TikTok.icon},
           {text: Platform.Services.Instagram.label, color: "InstagramColor", icon: Platform.Services.Instagram.icon},
           {text: Platform.Services.Other.label, color: "OtherServiceColor", icon: Platform.Services.Other.icon},
-      ]
+      ],
+      items: [
+            { color: '#6441a5' },
+            { color: '#fb5824' },
+            { color: '#0273ff' },
+            { color: '#0f1419' },
+            { color: '#c4302b' },
+            { color: '#06c755' },
+            { color: '#fe2c55' },
+            { color: '#cf2e92' },
+            { color: '#6f4b3e' },
+        ],
+        hoveredIndex: null
   }),
+  methods: {
+       itemStyle(index) {
+            return {
+                backgroundColor: this.hoveredIndex === index ? this.items[index].color : 'transparent'
+            };
+        }
+  }
 }
 </script>
 
@@ -50,6 +73,24 @@ export default {
         width: 48px;
         height: 48px;
         margin: 8px;
+        -webkit-transition: all 0.3s ease;
+        -moz-transition: all 0.3s ease;
+        -o-transition: all 0.3s ease;
+        transition: all  0.3s ease;
+        background-color: none;
+    }
+
+    .itemContainer:hover {
+        border-radius: 50%;
+        color: white;
+    }
+
+    .itemContainer:hover .category_underline {
+        background-color: white;
+        -webkit-transition: all 0.3s ease;
+        -moz-transition: all 0.3s ease;
+        -o-transition: all 0.3s ease;
+        transition: all  0.3s ease;
     }
 
     .category_name {
