b6353814f84b7b6d688d6189f189ba9362fc850e
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-06-19 21:25:01 +0900

    fix: inputフォームの置き換え

--- niconico-researcher/src/components/SearchForm.vue
+++ niconico-researcher/src/components/SearchForm.vue
@@ -2,17 +2,33 @@
     <div class="search_form_view" contenteditable="true">
         <div class="search_form_input_area">
             <i class="fa-solid fa-magnifying-glass"></i>
-            検索したい配信者の名前を入力
+            <input v-model="message" placeholder="検索したい配信者の名前を入力" />
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
+    input {
+        margin: 0;
+        padding: 0;
+        background: none;
+        border: none;
+        border-radius: 0;
+        outline: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        width: 100%;
+        height: 100%;
+        margin-right: 16px;
+    }
+
     .search_form_view {
         width: 358px;
         height: 48px;
         background-color: lightgray;
         margin: 0 16px 16px 16px;
+        padding-right: 8px;
     }
 
     .search_form_input_area {
@@ -23,6 +39,7 @@
         justify-content: left;
         margin-left: 8px;
         padding-left: 8px;
+        position: relative;
     }
 
     .search_form_input_area i {
