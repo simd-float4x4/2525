cad6958b820d937b74baa92454e74ec83b7398d6
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-06-20 22:46:18 +0900

    fix: 配信中の人が上になるように

--- niconico-researcher/src/components/Pagination/Pagination.vue
+++ niconico-researcher/src/components/Pagination/Pagination.vue
@@ -46,13 +46,23 @@ export default {
             var startIdx = (this.curPage - 1) * this.CellNumPerPage;
             var endIdx   = startIdx + this.CellNumPerPage;
     
-            var fruits = [];
+            var fruits = [];　// 全部の結果
+
             for(var i in this.Cells) {  //this.fruitsはdataで定義しているもの　3つのデータをforで順番に回していきます
                 var user  = this.Cells[i];  //回されてきたデータを変数foodに格納
                 if(user.userName.indexOf(this.keyword) !== -1) { //ここでfood.name(オレンジやメロン)とkeyword(ユーザーが入力した文字)が一致するか判断
                     fruits.push(user); //一致するなら配列fruitsにデータを格納
                 } 
             }
+
+            //　配信中の人が上になるように
+            fruits.sort(function(a, b) {
+                if (a.platform[0].isBroadCasting < b.platform[0].isBroadCasting) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
 
             // 末尾のページ番号
             this.pageNum =  Math.ceil(fruits.length / this.CellNumPerPage);
