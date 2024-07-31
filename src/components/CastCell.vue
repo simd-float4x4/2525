1092e359c04db3088124b43a1fbe159ad1791e71
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-07-31 19:27:58 +0900

    fix

--- src/components/CastCell.vue
+++ src/components/CastCell.vue
@@ -48,7 +48,7 @@ export default {
         thisCellPlatformText: "",
       }
   },
-  created() {
+  updated() {
       this.thisCellPlatformName = this.userData.platform.platformName;
       this.thisCellPlatformText = this.userData.platform.accountUserSubText;
       // this.printOutDebugData();
