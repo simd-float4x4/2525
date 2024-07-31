95fd847d7699c6520e474198f3a0240725e3cde6
simd-float4x4 <asobu.developers+Mathew@gmail.com>
2024-07-31 20:36:28 +0900

    fix

--- src/components/CastCell.vue
+++ src/components/CastCell.vue
@@ -17,7 +17,7 @@
         <div class="cell_user_name_area"
         @click="moveToUserStreamingURL( userData.platform.accountUserUrl )">
             <div class="user_name" :style="cellViewBackground(userData.platform.platformId)">{{ userData.platform.accountUserName }}</div>
-            <div class="user_id_label" :key="renderKey"> 
+            <div class="user_id_label"> 
                 {{ thisCellPlatformName + ': ' + thisCellPlatformText }} <br>
                 <div class="url">
                     
@@ -45,18 +45,16 @@ export default {
   data() {
       return {
         thisCellPlatformName: "",
-        thisCellPlatformText: "",
-        renderKey: 0,
+        thisCellPlatformText: ""
       }
   },
-  computed: {
-      // this.printOutDebugData();
-  }, 
+  created() {
+      this.updateSubText();
+  },
   methods: {
       updateSubText() {
         this.thisCellPlatformName = this.userData.platform.platformName;
         this.thisCellPlatformText = this.userData.platform.accountUserSubText;
-        this.renderKey += 1; 
       },
       printOutDebugData() {
           console.log(`platformName: ${this.userData.platform.platformName}`);
